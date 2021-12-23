d60647ec0065689a1cdbca219064c0123a65a98d
v79 <liamjdavison@gmail.com>
2021-12-23 17:05:21 +0000

    Initial commit of some Korge experiments

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+	id("com.soywiz.korge")
+	kotlin("kapt") version "1.5.21"
+	kotlin("plugin.serialization") version "1.5.31"
+}
+
+korge {
+	targetJvm()
+    targetJs()
+}
+
+repositories {
+	maven("https://jitpack.io")
+}
+
+val kiwiVersion: String by project
+val kloggerVersion = "2.2.0"
+val kotlinxSerializationVersion = "1.3.1"
+
+kotlin {
+	sourceSets {
+		val commonMain by getting {
+			kotlin.srcDir("$buildDir/generated/source/kaptKotlin/main")
+			dependencies {
+				implementation("com.soywiz.korlibs.klogger:klogger:$kloggerVersion")
+			}
+		}
+
+		val jvmMain by getting {
+			dependencies {
+				configurations.all { // kapt has an issue with determining the correct KMM library, so we need to help it
+					if (name.contains("kapt")) {
+						attributes.attribute(
+							org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType.attribute,
+							org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType.jvm // pass in the JVM
+						)
+					}
+				}
+			}
+		}
+	}
+}
+
+tasks.getByName("compileKotlinMetadata").dependsOn("kaptKotlinJvm")
+
+tasks {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        google()
+        maven("https://plugins.gradle.org/m2/")
+    }
+
+    val korgePluginVersion: String by settings
+
+    plugins {
+        id("com.soywiz.korge") version korgePluginVersion
+    }
+
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.namespace == "com.soywiz") {
+                useModule("com.soywiz.korlibs.korge.plugins:korge-gradle-plugin:$korgePluginVersion")
+            }
+        }
+    }
+}
